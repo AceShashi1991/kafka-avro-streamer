cc570b80b4d964d301d1ca1925db183a1bbd1259
Shashi <shashi52.699@gmail.com>
2022-05-21 00:53:46 +0530

    1. avro dependency added

--- src/test/java/com/shashi/kafkaavro/KafkaAvroExampleApplicationTests.java
+++ src/test/java/com/shashi/kafkaavro/KafkaAvroExampleApplicationTests.java
@@ -1,9 +1,14 @@
 package com.shashi.kafkaavro;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
 class KafkaAvroExampleApplicationTests {
 
 	@Test
